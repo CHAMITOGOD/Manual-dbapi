b9b1951480b01617923b81dcb2a3d2073ed4e6ce
AXELL ABRAHAM LOPEZ LEYVA <2023030531@upsin.edu.mx>
2026-04-13 23:42:44 -0700

    correccion

--- module/model.js
+++ module/model.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2";
 
-var conexion = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+const conexion = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 
